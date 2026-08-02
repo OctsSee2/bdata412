d3e4c6c1dff67befff3b0fd1f24e0d486f17ae42
ta-o666 <ll.gojicak.ll@gmail.com>
2026-08-01 19:55:07 -0700

    okay there is a histogram with a dropdown menu to choose the column to graph distribution of

--- app.R
+++ app.R
@@ -1,17 +1,78 @@
 library(shiny)
+library(ggplot2)
 
 # some stuff to load the data from 2024-2025 (testing)
 employment_data <- read.csv("./all_data_M_2025.csv")
 
 ui <- fluidPage(
   titlePanel("Testing testing"),
-  tableOutput("head_of_table")
+  sidebarLayout(
+    sidebarPanel(
+      selectInput(
+        inputId = "choice_var",
+        label = "Choose a column",
+        choices = c(
+          "Area" = "AREA",
+          "Area title" = "AREA_TITLE",
+          "Area type" = "AREA_TYPE",
+          "Primary state" = "PRIM_STATE",
+          "NAICS code" = "NAICS",
+          "NAICS title" = "NAICS_TITLE",
+          "something" = "I_GROUP",
+          "something else" = "OWN_CODE",
+          "another thing" = "OCC_CODE",
+          "what" = "O_GROUP",
+          "Total employment" = "TOT_EMP",
+          "????" = "EMP_PRSE",
+          "numbers" = "JOBS_1000",
+          "words" = "LOC_QUOTIENT",
+          "idk" = "PCT_TOTAL",
+          "idk either" = "PCT_RPT",
+          "so mean" = "H_MEAN",
+          "also mean" = "A_MEAN",
+          "still mean" = "MEAN_PRSE",
+          "numbers & words" = "H_PCT10",
+          "numbers & bigger words" = "H_PCT25",
+          "the middle" = "H_MEDIAN",
+          "numbers & even bigger words" = "H_PCT75",
+          "number & biggest word" = "H_PCT90",
+          "something another" = "A_PCT10",
+          "something another (bigger)" = "A_PCT25",
+          "something in the middle" = "A_MEDIAN",
+          "something another (even bigger)" = "A_PCT75",
+          "something another (biggest)" = "A_PCT90",
+          "annual ?" = "ANNUAL",
+          "hourly ?" = "HOURLY"
+        )
+      ),
+      sliderInput(
+        inputId = "bins",
+        label = "Number of bins:",
+        min = 1,
+        max = 50,
+        value = 30
+      )
+    ),
+    mainPanel(
+      plotOutput(outputId = "the_histogram_thing")
+    )
+  )
+  #tableOutput("head_of_table")
 )
 
 server <- function(input, output) {
-  output$head_of_table <- renderTable({
-    head(employment_data, n=10)
+  output$the_histogram_thing <- renderPlot({
+    selected_data <- employment_data[[input$choice_var]]
+    
+    ggplot(employment_data, aes_string(x = input$choice_var)) +
+      geom_histogram(bins = input$bins,
+                     fill = "red", color = "black") +
+      labs(title = paste("A graph for `", input$choice_var, "`")) +
+      theme_minimal()
   })
+#  output$head_of_table <- renderTable({
+#    head(employment_data, n=10)
+#  })
 }
 
 #ui <- fluidPage(
@@ -26,7 +87,6 @@ server <- function(input, output) {
 #                        max = 50,
 #                        value = 30)
 #        ),
-
 #        mainPanel(
 #           plotOutput("distPlot")
 #        )
