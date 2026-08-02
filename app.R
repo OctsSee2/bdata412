74299cb45239fc968a3709c8e0a3fb3fa163e789
ta-o666 <ll.gojicak.ll@gmail.com>
2026-08-01 20:43:00 -0700

    added data dictionary + my understanding of each column of the dataset

--- app.R
+++ app.R
@@ -57,7 +57,6 @@ ui <- fluidPage(
       plotOutput(outputId = "the_histogram_thing")
     )
   )
-  #tableOutput("head_of_table")
 )
 
 server <- function(input, output) {
@@ -70,40 +69,6 @@ server <- function(input, output) {
       labs(title = paste("A graph for `", input$choice_var, "`")) +
       theme_minimal()
   })
-#  output$head_of_table <- renderTable({
-#    head(employment_data, n=10)
-#  })
 }
-
-#ui <- fluidPage(
-#    titlePanel("Testing employment data from 2024-2025"),
-#
-#    # Sidebar with a slider input for number of bins 
-#    sidebarLayout(
-#        sidebarPanel(
-#            sliderInput("bins",
-#                        "Number of bins:",
-#                        min = 1,
-#                        max = 50,
-#                        value = 30)
-#        ),
-#        mainPanel(
-#           plotOutput("distPlot")
-#        )
-#    )
-#)
-
-#server <- function(input, output) {
-#    output$distPlot <- renderPlot({
-#        # generate bins based on input$bins from ui.R
-#        x    <- employment_data[, 2]
-#        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-#        # draw the histogram with the specified number of bins
-#        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-#             xlab = 'Something ????',
-#             main = 'Histogram of something ????')
-#    })
-#}
 
 shinyApp(ui = ui, server = server)
